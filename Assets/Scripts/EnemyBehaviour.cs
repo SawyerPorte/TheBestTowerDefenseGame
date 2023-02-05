c3c55051782a10ff133e885a69d66ac6510a6bf1
DuangcaBey <69988421+DuangcaBey@users.noreply.github.com>
2023-02-04 23:29:08 -0800

    Enemy Sprites

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_N_Behaviour : MonoBehaviour
+public class EnemyBehaviour : MonoBehaviour
 {
     [Header("Enemy Prefabs")]
-    [SerializeField] GameObject Enemy_N;
+    [SerializeField] GameObject Enemy;
 
     [SerializeField] Transform[] waypoints;
     [SerializeField] float moveSpeed = 1f;
@@ -14,7 +14,7 @@ public class Enemy_N_Behaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Enemy_N.transform.position = waypoints[wpIndex].transform.position;
+        Enemy.transform.position = waypoints[wpIndex].transform.position;
     }
 
     // Update is called once per frame
@@ -25,8 +25,8 @@ public class Enemy_N_Behaviour : MonoBehaviour
 
     void Enemy_Nmove()
     {
-        Enemy_N.transform.position = Vector2.MoveTowards(transform.position, waypoints[wpIndex].transform.position, moveSpeed * Time.deltaTime);
-        if (Enemy_N.transform.position == waypoints[wpIndex].transform.position)
+        Enemy.transform.position = Vector2.MoveTowards(transform.position, waypoints[wpIndex].transform.position, moveSpeed * Time.deltaTime);
+        if (Enemy.transform.position == waypoints[wpIndex].transform.position)
         {
             wpIndex++;
         }
